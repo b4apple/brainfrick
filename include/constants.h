ce5c8be8cc4e802a67310b4652610d83693563de
Aritro Shome <aritro.shome.mpb@gmail.com>
2021-10-04 15:06:39 +0530

    code : added help message to constants.h and handling out-of bound arguments in main file

--- include/constants.h
+++ include/constants.h
@@ -5,4 +5,4 @@
 #define BF_EXIT_ON_FILE_ERROR 3
 #define BF_EXIT_ON_RUNTIME_ERROR 4
 #define BF_EXIT_ON_LANG_ERROR 5 
-
+#define BF_USAGE_TEXT "USAGE : bfcc.exe [FILENAME] [BACKEND-CHOICE]\n\tFILENAME: is a file with .bf extension.\n\tIf no extension is specified, then the compiler appends .bf to the filename and tries to open the file.\n\n\tBACKEND-CHOICE: (Optional) The choices include C, WASM, interp.\n\tIf nothing is specified it is set to interp by default."
--- src/main.c
+++ src/main.c
@@ -1,8 +1,11 @@
 #include "constants.h" 
+#include "utils.h"
 #include <stdio.h>
 
-int main(int argc, char const *argv[])
-{
-	
-	return 0;
+int main(int argc, char const *argv[]) {
+	if (argc == 1 || argc > 4) {
+		printf(BF_USAGE_TXT);
+		exit(BF_EXIT_ON_RUNTIME_ERROR);
+	}
+	exit(BF_EXIT_ON_SUCCESS);
 }
